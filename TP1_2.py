5a5e722291e493f73ace6654423e6ae96a9fa3b9
junnchy <juanichacho@gmail.com>
2020-05-01 22:20:41 -0300

    cambios extranios que hay en mi

--- TP1_2.py
+++ TP1_2.py
@@ -1,146 +1,327 @@
 from random import randint
-import pandas as pd 
+import pandas as pd
 import matplotlib.pyplot as plt
-from math import pow
-from math import sqrt
 import numpy as np
 
+
 class Ruleta(object):
-    
+
     def __init__(self):
+        # Los arreglos terminados en A son arreglos de arreglos
         self.resultados = []
-        self.frecuenciaRelativa = []
-        self.vp = []
-        self.varianzas = []
-        self.desvios = []
-        self.nroTirada =[]
-        self.ocurrencias = 0
-        self.acumRelativas = 0
-        self.acumResultados = 0
-        self.promedio = 0
-        self.acumVar = 0
-        self.varEsp = 0
-        self.desEsp = 0
+        self.apuestas = []
+        self.ganadores = {}
+        self.listaGanadores = []
+        self.tiradas = []
+        self.paga2 = ['par', 'impar', 'm-1', 'm-2', 'rojo', 'negro']
+        self.paga1_5 = ['c-1', 'c-2', 'c-3', 'd-1', 'd-2', 'd-3']
+        self.pagos = []
+        self.panio = {
+            'paridad': {
+                'par': [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36],
+                'impar': [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35]
+            },
+            'mitades': {
+                '1': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18],
+                '2': [19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36]
+            },
+            'color': {
+                'rojo': [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36],
+                'negro': [2, 4, 5, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35]
+            },
+            'docenas': {
+                '1': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+                '2': [13, 14, 15, 16, 17, 18, 9, 20, 21, 22, 23, 24],
+                '3': [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36]
+            },
+            'columnas': {
+                '1': [1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31, 34],
+                '2': [2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32, 35],
+                '3': [3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36]
+            },
+        }
 
-    def tirar(self, n, esperado):
-        self.tiradas = n
-        self.esperado = esperado
-        for i in range(n+1):
-            if i == 0:
-                pass
-            else:
-                x = randint(0, 36)
-                self.resultados.append(x)
-                self.frt(i, x, esperado)
-                self.vpt(i,x)
-                self.varianza(i,x)
-        print('Resultados: ', self.resultados)
-        print(len(self.nroTirada), len(self.vp), len(self.frecuenciaRelativa))
-        print(pd.DataFrame(self.frecuenciaRelativa))
-        self.fa()
-        self.calculosFinales()
-        self.imprimirGraficos()
+    def jugar(self, n):
+        self.definirGanador(self.tirar(n))
 
-    #Fecuencia por tirada
-    def frt(self, i , x, esperado):
-        if x == esperado:
-            self.ocurrencias = self.ocurrencias + 1
-        if self.ocurrencias != 0:
-            aux = self.ocurrencias/i
+    def tomarApuestas(self, apuestas):
+        if apuestas == [None]:
+            self.apuestas = []
         else:
-            aux = 0
-        self.frecuenciaRelativa.append(aux)
-        self.nroTirada.append(i)
-        self.acumRelativas = aux + self.acumRelativas
+            self.apuestas = []
+            for apuesta in apuestas:
+                self.apuestas.append(apuesta)
 
-    #Valor Promedio de las tiradas
-    def vpt(self, i, x):
-        self.acumResultados = self.acumResultados + x
-        self.vp.append(self.acumResultados/i)
+    def tirar(self, n):
+        x = randint(0, 36)
+        self.resultados.append(x)
+        self.tiradas.append(n)
+        return x
+
+    def definirGanador(self, nro):
+        ganadores = []
+        if nro == 0:
+            ganadores.append(nro)
+        else:
+            # Paridad
+            if nro in self.panio['paridad']['par']:
+                ganadores.append('par')
+            elif nro in self.panio['paridad']['impar']:
+                ganadores.append('impar')
+
+            # Mitades
+            if nro in self.panio['mitades']['1']:
+                ganadores.append('m-1')
+            elif nro in self.panio['mitades']['2']:
+                ganadores.append('m-2')
+
+            # Colores
+            if nro in self.panio['color']['rojo']:
+                ganadores.append('rojo')
+            elif nro in self.panio['color']['negro']:
+                ganadores.append('negro')
+
+            # Docenas
+            if nro in self.panio['docenas']['1']:
+                ganadores.append('d-1')
+            elif nro in self.panio['docenas']['2']:
+                ganadores.append('d-2')
+            elif nro in self.panio['docenas']['3']:
+                ganadores.append('d-3')
+
+            # Columnas
+            if nro in self.panio['columnas']['1']:
+                ganadores.append('c-1')
+            elif nro in self.panio['columnas']['2']:
+                ganadores.append('c-2')
+            elif nro in self.panio['columnas']['3']:
+                ganadores.append('c-3')
+
+            self.ganadores = ganadores
+            self.listaGanadores.append(ganadores)
+
+    def pagar(self):
+        self.pagos = []
+        if self.apuestas is not None:
+            for apuesta in self.apuestas:
+                if apuesta['apuesta'] in self.ganadores:
+                    if apuesta['apuesta'] in self.paga2:
+                        aux = apuesta['cantidad'] * 2
+                        self.pagos.append({'pago': aux, 'nombre': apuesta['nombre']})
+                    elif apuesta['apuesta'] in self.paga1_5:
+                        aux = apuesta['cantidad'] * 1.5
+                        self.pagos.append({'pago': aux, 'nombre': apuesta['nombre']})
+                else:
+                    self.pagos.append({'pago': 0, 'nombre': apuesta['nombre']})
+            return self.pagos
+        else:
+            pass
+            # print('No se recibieron apuestas')
 
 
-    #Valor del desvio
-    def desvio(self, var):
-        self.desvios.append(sqrt(var))
+class Jugador(object, ):
 
-    #Valor de la varianza
-    def varianza(self, i, x):
-        aux = pow((x-(self.acumResultados/i)),2)
-        self.acumVar = self.acumVar + aux
-        aux2 = self.acumVar/i
-        self.varianzas.append(aux2)
-        self.desvio(aux2)
+    def __init__(self, nombre, tc, jugada, aque):
+        self.capital = 10000000
+        self.nombre = nombre
+        self.apuesta = 1
+        self.evolucionCapital = []
+        self.ganancias = []
+        self.resultados = []
+        self.apuestas = []
+        self.nroJuego = []
+        self.ganadoxt = []
+        self.ganados = 0
+        self.perdidos = 0
+        self.jugada = jugada
+        self.aque = aque
+        self.tc = tc  # Tipo de capital 0 limitado 1 ilimitado
+        self.juega = True
+        self.j = 0
 
-    #Frecuencia absoluta de cada uno de los resultados
-    def fa(self):
-        a = pd.Series(self.resultados).value_counts()
-        b = pd.DataFrame(a)
-        b.columns = ['Cantidad']
-        print(b.loc[[self.esperado]])
+    def continua(self):
+        return self.juega
 
-    #calculos finales
-    def calculosFinales(self):
-         #Calculo para la FR
-        self.acumRelativas = self.acumRelativas / self.tiradas
+    def apostarMartingala(self, valor):
+        if self.juega:
+            if self.capital >= self.apuesta:
+                self.j += 1
+                self.nroJuego.append(self.j)
+                self.capital -= self.apuesta
+                self.apuestas.append(self.apuesta)
 
-        #Calculo del Promedio General
-        aux = 0
-        for i in range(37):
-            aux = aux + i
-        self.promedio = aux/37
+                return {
+                    'apuesta': valor,
+                    'cantidad': self.apuesta,
+                    'nombre': self.nombre
+                }
+            else:
+                # print('Ya no queda plata')
+                self.juega = False
+        else:
+            pass
 
-        aux2 = 0
-        for i in range(37):
-            aux2 = aux2 + pow((i - self.promedio), 2)
-        self.varEsp = aux2/37
-        self.desEsp = sqrt(self.varEsp)
+    def tomarGanancia(self, ganancia):
+        if self.juega:
+            for pago in ganancia:
+                if pago['nombre'] == self.nombre:
+                    if pago['pago'] == 0:
+                        self.resultados.append('perdi')
+                        self.perdidos += 1
+                        self.apuesta = self.apuesta * 2
+                        if self.tc == 1:
+                            self.capital = self.capital * 2  # Capital limitado
+                        else:
+                            pass
+                    else:
+                        self.capital = (self.capital - self.apuesta) + pago['pago']
+                        self.resultados.append('gane')
+                        self.ganados += 1
 
-        print('Resultados')
-        print('Ultima Fecuencia Relativa: ', self.frecuenciaRelativa[self.tiradas - 1],'esperada:', self.acumRelativas)
-        print('Ultimo Promedio: ', self.vp[self.tiradas - 1],'esperado:', self.promedio)
-        print('Ultima Varianza: ', self.varianzas[self.tiradas - 1],'esperada:', self.varEsp)
-        print('Ultimo Desvio: ', self.desvios[self.tiradas - 1],'esperado:', self.desEsp)
+                    self.ganancias.append(pago['pago'])
+                    self.evolucionCapital.append(self.capital)
+                    # self.mostarResultados()
+                else:
+                    pass
+            else:
+                pass
 
+    def mostarResultados(self):
+        # print('Capital:', self.capital)
+        # print('Evolucion Capital: ', self.evolucionCapital)
+        print('Resultados: ', self.resultados)
+        print('Ganancias: ', self.ganancias)
+        print('Apuestas: ', self.apuestas)
+        print('1...', self.nroJuego)
+        print('Ganados:', self.ganados)
+        print('Perdidos', self.perdidos)
+
+    def defineJuagda(self):
+        if self.jugada == 1:
+            return self.apostarMartingala(self.aque)
+
+    def devolverResultados(self):
+        if self.tc == 1:
+            return [self.apuestas, self.ganancias, self.nroJuego, [self.ganados, self.perdidos]]
+        else:
+            return [self.apuestas, self.ganancias, self.nroJuego, [self.ganados, self.perdidos], self.evolucionCapital]
+
+    def sayName(self):
+        if self.jugada == 1:
+            return self.nombre, 'Martingala', self.aque
+
+    def sayNombre(self):
+        return self.nombre
 
     def imprimirGraficos(self):
-
-        #Graficos
-        plt.title('Graficos')
+        print(len(self.nroJuego), self.nroJuego)
+        print(len(self.evolucionCapital), self.evolucionCapital)
+        print(len(self.apuestas), self.apuestas)
 
         plt.subplot(2, 2, 1)
-        plt.plot(self.nroTirada, self.frecuenciaRelativa, color='darkblue')
-        plt.axhline(y=self.acumRelativas, color='r', linestyle='-')
+        plt.plot(self.nroJuego, self.apuestas, color='darkblue')
         plt.xlabel('n (numero de tiradas)')
-        plt.ylabel('fr (frecuencia relativa)')
+        plt.ylabel('Apuestas')
 
         plt.subplot(2, 2, 2)
-        plt.plot(self.nroTirada, self.vp, color='darkorange')
-        plt.axhline(y=self.promedio, color='r', linestyle='-')
+        plt.plot(self.nroJuego, self.ganancias, color='darkblue')
         plt.xlabel('n (numero de tiradas)')
-        plt.ylabel('VP(Valor Promedio de las Tiradas)')
+        plt.ylabel('Ganancias')
 
+        # plt.subplot(2, 2, 3)
+        # plt.plot(self.nroJuego, self.evolucionCapital, color='darkblue')
+        # plt.xlabel('n (numero de tiradas)')
+        # plt.ylabel('CAPITAL')
 
-        plt.subplot(2, 2, 3)
-        plt.plot(self.nroTirada, self.desvios, color='forestgreen')
-        plt.axhline(y=self.desEsp, color='r', linestyle='-')
-        plt.xlabel('n (numero de tiradas)')
-        plt.ylabel('Desvio')
-
-        plt.subplot(2, 2, 4)
-        plt.plot(self.nroTirada, self.varianzas, color='darkorchid')
-        plt.axhline(y=self.varEsp, color='r', linestyle='-')
-        plt.xlabel('n (numero de tiradas)')
-        plt.ylabel('Varianza')
-
-        plt.savefig('full_figure_one.png')
         plt.show()
 
 
-
 def main():
+    resultados = []
+    colores = ['gold', 'darkblue', 'darkorange', 'forestgreen', 'darkorchid', 'violet', 'dark']
     r = Ruleta()
-    r.tirar(10000, 5)
+    # Instancias de Jugador (Primer parametro) => Nombre
+    # Instancias de Jugador (Segundo parametro) => 0 para capital limitado 1 para ilimitado
+    # Instancias de Jugador (Tercer parametro) => Metodologia de apuesta
+    # instancias de jugador (Cuarto parametro) => A que apuesta
+    j1 = Jugador('j1', 0, 1, 'par')
+    j2 = Jugador('j2', 1, 1, 'impar')
+    j3 = Jugador('j3', 0, 1, 'd-3')
+
+    jugadores = [j1, j2, j3]
+
+    for n in range(5):
+        apuestas = []
+        for j in jugadores:
+            apuestas.append(j.defineJuagda())
+        r.tomarApuestas(apuestas)
+        r.jugar(n)
+        for j in jugadores:
+            print(r.pagar())
+            j.tomarGanancia(r.pagar())
+    for j in jugadores:
+        resultados.append(j.devolverResultados())
+    print(resultados)
+
+    plt.subplot(2, 2, 1)
+    aux2 = 0
+    for r in jugadores:
+        aux = r.devolverResultados()
+        plt.plot(aux[2], aux[0], label=r.sayName(), color=colores[aux2])
+        aux2 += 1
+    plt.grid(True)
+    plt.xlabel('n (numero de tiradas)')
+    plt.ylabel('Apuestas')
+    plt.legend(loc="upper left")
+    plt.title('Apuestas')
+
+    plt.subplot(2, 2, 2)
+    plt.tight_layout()
+    aux2 = 0
+    for r in jugadores:
+        aux = r.devolverResultados()
+        plt.plot(aux[2], aux[1], label=r.sayName(), color=colores[aux2])
+        aux2 += 1
+    plt.grid(True)
+    plt.xlabel('n (numero de tiradas)')
+    plt.ylabel('Ganancias')
+    plt.legend(loc="upper left")
+    plt.title('Ganancias (no acumulativas)')
+
+    plt.subplot(2, 2, 3)
+    aux2 = 0
+    for r in jugadores:
+        aux = r.devolverResultados()
+        if len(aux) > 4:
+            plt.plot(aux[2], aux[4], label=r.sayName(), color=colores[aux2])
+            aux2 += 1
+        else:
+            pass
+    plt.grid(True)
+    plt.xlabel('n (numero de tiradas)')
+    plt.ylabel('CAPITAL')
+    plt.legend(loc="upper left")
+    plt.title('Evolucion de Capital')
+
+    plt.subplot(2, 2, 4)
+    nombres = []
+    ganados = []
+    for r in jugadores:
+        aux = r.devolverResultados()
+        nombres.append(r.sayNombre())
+        ganados.append(aux[3][0])
+    y_pos = np.arange(len(nombres))
+    plt.bar(y_pos, ganados, align='center', alpha=0.5)
+    plt.title('Ganados Por Jugador')
+    plt.xticks(y_pos, nombres)
+
+    x = randint(10000, 99999)
+    nombre = 'figura_TP_1_2' + str(x) + '.png'
+    plt.savefig(nombre)
+
+    plt.show()
 
 
- 
+
+
 main()
