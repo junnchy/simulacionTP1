3e41231bcf21fe1d2d49d80f2d1afc306d558ea5
junnchy <juanichacho@gmail.com>
2020-05-15 19:00:23 -0300

    test series

--- TP2.py
+++ TP2.py
@@ -1,10 +1,10 @@
 import pandas as pd
-import openpyxl
-import clipboard as clp
+from random import randint
+
 
 class Metodos(object):
 
-    def __init__(self, s, a ,m, c):
+    def __init__(self, s, a, m, c):
         self.semilla = s
         self.a = a
         self.m = m
@@ -16,43 +16,122 @@ class Metodos(object):
         self.acC = []
         self.acGCL = []
 
+        # arreglos para test
+        self.paresGCL = []
+        self.paresC = []
 
     def gcl(self):
-        for i in range(self.m -1):
-            y = self.a * self.acumulaGCL[i] % self.m
+        for i in range(self.m):
+            y = (self.a * self.acumulaGCL[i]) % self.m
             self.acumulaGCL.append(y)
-            self.acGCL.append(self.acumulaGCL[-1]/self.m)
+            self.acGCL.append(self.acumulaGCL[-1] / self.m)
         return self.acGCL
 
     def cuadrados(self):
         if int(len(str(self.semilla))) % 2 == 0:
             if len(str(self.semilla)) < self.c:
-                print ('Se requiere una semilla de mas de ', self.c, ' digitos')
+                print('Se requiere una semilla de mas de ', self.c, ' digitos')
             else:
                 a = self.c / 2
                 b = (self.c * 2) - a
-                for i in range(self.m -1):
-                    x = self.acumulaC[i]**2
-                    while len(str(x)) < self.c*2:
+                for i in range(self.m):
+                    x = self.acumulaC[i] ** 2
+                    while len(str(x)) < self.c * 2:
                         x = '0' + str(x)
                     self.acumulaC.append(int(str(x)[int(a):int(b):]))
-                    self.acC.append(self.acumulaC[-1]/self.m)
+                    self.acC.append(self.acumulaC[-1] / self.m)
             return self.acC
         else:
             print('Ingrese un numero de largo par, de ', self.c, ' digitos, para el metodo de cuadrados')
 
+    def test_series(self, arrayGCL, arrayC):
+        n = 10
+        ejex = []
+        ejey = []
+        rC = {}
+        rG = {}
+        rR = {}
+        rx = {}
+        ry = {}
+        for i in range(n):
+            if i == 0:
+                aux1 = 0
+            else:
+                aux1 = i/n
+            aux2 = (i+1)/n
+            nombrex = 'x'+ str(i)
+            for j in range(n):
+                nombrey = 'y'+ str(j)
+                nombre = nombrex + nombrey
+                rG[nombre] = 0
+                rC[nombre] = 0
+                rR[nombre] =0
+
+            ny = 'y'+ str(i)
+            ejex.append([aux1, aux2, nombrex])
+            ejey.append([aux1, aux2, ny])
+            rx[nombrex] = 0
+            ry[ny] = 0
+
+        random = []
+        for i in range(self.m):
+            random.append(randint(0,100)/100)
+
+        paresRandom = []
+
+        for i in range(int(len(arrayC) - 1)):
+            self.paresC.append([arrayC[i], arrayC[i + 1]])
+
+        for i in range(int(len(arrayGCL) - 1)):
+            self.paresGCL.append([arrayGCL[i], arrayGCL[i + 1]])
+
+        for i in range(int(len(random) - 1)):
+            paresRandom.append([random[i], random[i + 1]])
+
+
+        for par in self.paresC:
+            for valx in ejex:
+                if valx[0] <= par[0] < valx[1]:
+                    rx[valx[2]] += 1
+                    for valy in ejey:
+                        if valy[0] <= par[1] < valy[1]:
+                            ry[valy[2]] += 1
+                            k = valx[2] + valy[2]
+                            rC[k] = rC[k] + 1
+
+        for par in self.paresGCL:
+            for valx in ejex:
+                if valx[0] <= par[0] < valx[1]:
+                    rx[valx[2]] += 1
+                    for valy in ejey:
+                        if valy[0] <= par[1] < valy[1]:
+                            ry[valy[2]] += 1
+                            k = valx[2] + valy[2]
+                            rG[k] = rG[k] + 1
+
+        for par in paresRandom:
+            for valx in ejex:
+                if valx[0] <= par[0] < valx[1]:
+                    for valy in ejey:
+                        if valy[0] <= par[1] < valy[1]:
+                            k = valx[2] + valy[2]
+                            rR[k] = rR[k] + 1
+
+        print(self.paresC)
+        print(self.paresGCL)
+        print(ejex)
+        print(ejey)
+        print('test cudadrados', rC)
+        print('test Congruencial', rG)
+        print('random', rR)
+
 
 def main():
     mgcl = Metodos(52, 6, 100, 2)
-    # print(mgcl.gcl())
-    # print(mgcl.cuadrados())
     d = {'GCL': mgcl.gcl(), 'Cuadrados': mgcl.cuadrados()}
     df = pd.DataFrame(data=d)
     print(df)
+    mgcl.test_series(mgcl.gcl(), mgcl.cuadrados())
 
 
 main()
-
-
-
-
